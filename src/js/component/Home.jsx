85efe1b8196cba20e941e95d50eb194a92654e85
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2025-01-15 18:16:43 +0000

    [sp-93] class1: home inicial

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React from "react";  // 1. Importo React
+import rigoImage from "../../img/rigo-baby.jpg";  // Include images into your bundle
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+// 2. Create your first component with PascalCase name.
 const Home = () => {
+	// 3. Code JS
+
+	// 4. Retorno un solo y solo un elemento HTML
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<div className="text-center">
+				<h1 className="text-center mt-5">Hello Rigo!</h1>
+				<p>
+					<img src={rigoImage} />
+				</p>
+				<a href="#" className="btn btn-success">
+					If you see this green button... bootstrap is working...
+				</a>
+				<p>
+					Made by{" "}
+					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+					love!
+				</p>
+			</div>
 	);
 };
 
+
+// 5. Exporto mi componente
 export default Home;
+
+
+// En el archivo donde voy a utilizar el componente
+// 6 importar el componente
+// 7 utilizarlo
