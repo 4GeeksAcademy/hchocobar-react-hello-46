78fabea6a0ffe2ef45f0093998814ca8be7a08cd
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2025-01-15 19:33:24 +0000

    [sp-93] class1: jumbotron y button

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -3,6 +3,8 @@ import rigoImage from "../../img/rigo-baby.jpg";  // Include images into your bu
 // 6 importar el componente y 7 utilizar en otro componente
 import Navbar from "./Navbar.jsx";  // Componente con export default
 import { Footer } from "./Footer.jsx";  // Componente con export const
+import { Jumbotron } from "./Jumbotron.jsx";
+
 
 // 2. Create your first component with PascalCase name.
 const Home = () => {
@@ -12,13 +14,16 @@ const Home = () => {
 	return (
 			<div className="text-center">
 				<Navbar />
-				<h1 className="text-center mt-5">Hello Rigo!</h1>
+				<h1 className="text-center mt-5">{'Intro React'}</h1>
+				<Jumbotron />
+				{/* 
 				<p>
 					<img src={rigoImage} />
 				</p>
 				<a href="#" className="btn btn-success">
 					If you see this green button... bootstrap is working...
 				</a>
+				*/}
 
 				<Footer/>
 			</div>
